097cb1a60a1079db58d223ab9a694090043275f4
Aliou Barry <barryaliou980@gmail.com>
2025-04-01 02:43:27 -0400

    add facebook and twitter preview

--- src/app/(site)/(pages)/products/[id]/page.tsx
+++ src/app/(site)/(pages)/products/[id]/page.tsx
@@ -45,14 +45,35 @@ export async function generateMetadata(
     title: `${product?.title} | MonTech`,
     description: `Découvrez les détails du produit ${product?.title}. Prix: ${product?.price}€`,
     // pour le partage facebook et google 
-    openGraph: {
+    penGraph: {
       title: `${product?.title} | MonTech`,
       description: `Découvrez les détails du produit ${product?.title}. Prix: ${product?.price}€`,
       url: `https://montech.com/products/${product?.id}`,
       siteName: "MonTech",
       type: "website",
       locale: "fr_FR",
+      images: [
+        {
+          url: product.imgs?.previews[0], // URL de l'image
+          width: 1200,  // Largeur de l'image en pixels
+          height: 630,  // Hauteur de l'image en pixels
+          alt: `Image du produit ${product?.title}`,  // Texte alternatif pour l'image
+        },
+        {
+          url: product.imgs?.previews[1], // URL de l'image
+          width: 1200,  // Largeur de l'image en pixels
+          height: 630,  // Hauteur de l'image en pixels
+          alt: `Image du produit ${product?.title}`,  // Texte alternatif pour l'image
+        },
+      ],
     },
+    twitter: {
+      card: "summary_large_image",  // Type de carte (utiliser "summary_large_image" pour une grande image)
+      site: "@MonTEch",  // Nom d'utilisateur Twitter de votre site
+      title: `${product?.title} | MonTech`,
+      description: `Découvrez les détails du produit ${product?.title}. Prix: ${product?.price}€`,
+      image: product.imgs?.previews[0]
+    }
   }
 }
 
